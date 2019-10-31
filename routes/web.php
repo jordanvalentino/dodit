c206b48e3119731ee5b0fad15a3a05b268c39b2d
jordanvalentino <jordan.v.lomanto@gmail.com>
2019-10-31 13:35:28 +0700

    home -a

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,10 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('login');
 });
+
+Auth::routes();
+
+Route::resource('home', 'HomeController');
+// Route::resource('transaction', 'TransactionController');
