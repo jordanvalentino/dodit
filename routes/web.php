3d8a2249475ae77ff5036d9e0d48f20ba17fcdd8
jordanvalentino <jordan.v.lomanto@gmail.com>
2019-11-05 11:07:39 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,13 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::resource('home', 'HomeController');
+Route::resource('debit_category', 'DebitCategoryController');
+Route::resource('credit_category', 'CreditCategoryController');
+Route::resource('debit_transaction', 'DebitTransactionController');
+Route::resource('credit_transaction', 'CreditTransactionController');
+Route::resource('budget', 'BudgetController');
+Route::resource('detail', 'DetailController');
+Route::get('detail/create/{budget_id}', 'DetailController@create');
+Route::get('detail/{budget_id}/{id}/edit', 'DetailController@edit');
+Route::delete('detail/{budget_id}/{id}', 'DetailController@destroy');
 // Route::resource('transaction', 'TransactionController');
